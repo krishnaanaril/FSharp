92d59136efb85058eb7a12216bc6f33a4a5ef981
Krishna Mohan A M <krishnamohan.a.m@gmail.com>
2020-05-01 11:46:58 +0530

    add another two f# solutions

---
+++ HackerRank_FP/HR-FP-11.fs
@@ -0,0 +1,13 @@
+﻿//Evaluating e^x: https://www.hackerrank.com/challenges/eval-ex/problem
+module HackerRank
+
+open System
+
+[<EntryPoint>]
+let main argv =    
+    let limit = Console.ReadLine() |> int
+    let inputList  = List.init limit (fun elem -> (Console.ReadLine() |> float) )
+    let factorial n = [1..n] |> List.fold (*) 1 |> float
+    let exponential num = [1..10] |> List.mapi (fun i x -> ((pown num i)/factorial i)) |> List.fold (+) 0.0
+    inputList |> List.map exponential |> List.iter (printfn "%f")
+    0 // return an integer exit code
---
+++ HackerRank_FP/HR-FP-12.fs
@@ -0,0 +1,21 @@
+﻿//Area Under Curves and Volume of Revolving a Curve: https://www.hackerrank.com/challenges/area-under-curves-and-volume-of-revolving-a-curv/problem
+module HackerRank
+
+open System
+
+[<EntryPoint>]
+let main argv =    
+    let dataA = Console.ReadLine().Split(' ') |> Seq.map double
+    let dataB = Console.ReadLine().Split(' ') |> Seq.map double    
+    let limits = Console.ReadLine().Split(' ') |> Seq.map double |> Seq.toList
+    let steppers = seq { limits.Item(0) .. 0.001 .. limits.Item(1) }    
+    let fn dat = 
+        Seq.map2 (fun x y -> x * (dat ** y)) dataA dataB 
+        |> Seq.reduce (+)
+    let (area, volume) = 
+        steppers 
+        |> Seq.map (fun elem -> (fn elem) * 0.001, Math.PI * ((fn elem) ** 2.0) * 0.001) 
+        |> Seq.reduce (fun (a, b) (c, d) -> c+a, b+d)
+    printfn "%f" area
+    printfn "%f" volume
+    0 // return an integer exit code
